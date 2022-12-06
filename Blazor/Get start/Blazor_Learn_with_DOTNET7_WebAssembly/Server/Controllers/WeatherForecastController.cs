e53d6d48a618993b0430f58de8abea4335c8569b
MK VIMALAN <kiruvi5@gmail.com>
2022-12-06 20:22:41 +0530

    Added initial project file

---
+++ Blazor/Get start/Blazor_Learn_with_DOTNET7_WebAssembly/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using Blazor_Learn_with_DOTNET7_WebAssembly.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blazor_Learn_with_DOTNET7_WebAssembly.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
